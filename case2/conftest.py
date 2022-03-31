348da24a9c8aed5d0e3a45aba91b4a1731a3cb62
RomanZ <romanzayac1987@gmail.com>
2022-03-31 12:41:09 +0300

    change desired to options on driver section

--- case2/conftest.py
+++ case2/conftest.py
@@ -1,20 +1,31 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
 
 
 @pytest.fixture(scope="session")
 def setup(request):
-    capabilities = {
-        "browserName": "firefox",
-        "browserVersion": "98.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": False
-        }
-    }
+
+    # capabilities = {
+    #     "browserName": "firefox",
+    #     "browserVersion": "98.0",
+    #     "selenoid:options": {
+    #         "enableVNC": True,
+    #         "enableVideo": False
+    #     }
+    # }
+    firefox_options = webdriver.FirefoxOptions()
+    firefox_options.set_capability("browserVersion", "97.0")
+    firefox_options.set_capability("browserName", "firefox")
+    firefox_options.set_capability("enableVNC", True)
+    firefox_options.set_capability("enableVideo", False)
     driver = webdriver.Remote(
         command_executor="http://localhost:4444/wd/hub",
-        options=capabilities)
+        options=firefox_options)
+
+    # driver = webdriver.Remote(
+    #     command_executor="http://localhost:4444/wd/hub",
+    #     desired_capabilities=capabilities)
     driver.maximize_window()
 
     yield driver
