890216c11896322e7d0d636cc85bd8785caa3ad9
RomanZ <romanzayac1987@gmail.com>
2022-04-14 08:59:06 +0300

    added allure

--- case3/test_example1.py
+++ case3/test_example1.py
@@ -22,40 +22,6 @@ firefox98_cap={"browserName": "firefox",
        }
 
 
-# @pytest.fixture(scope="class")
-# def driver_init(request):
-#     ff_driver = webdriver.Remote(
-#     command_executor='http://localhost:4444/wd/hub',
-#     desired_capabilities=firefox97_cap)
-#     request.cls.driver = ff_driver
-#     yield
-#     ff_driver.close()
-#
-# @pytest.fixture(scope="class")
-# def chrome_driver_init(request):
-#     chrome_driver = webdriver.Remote(
-#     command_executor='http://localhost:4444/wd/hub',
-#     desired_capabilities=firefox98_cap)
-#     request.cls.driver = chrome_driver
-#     yield
-#     chrome_driver.close()
-# @pytest.mark.usefixtures("driver_init")
-# class BasicTest:
-#     pass
-# class Test_URL(BasicTest):
-#     def test_open_url(self):
-#         self.driver.get("https://www.lambdatest.com/")
-#         print(self.driver.title)
-#         time.sleep(5)
-#
-# @pytest.mark.usefixtures("chrome_driver_init")
-# class Basic_Chrome_Test:
-#     pass
-# class Test_URL_Chrome(Basic_Chrome_Test):
-#     def test_open_url(self):
-#         self.driver.get("https://www.lambdatest.com/")
-#         print(self.driver.title)
-#
 
 @pytest.fixture(params=["firefox_97", "firefox_98"],scope="class")
 def driver_init(request):
@@ -74,11 +40,6 @@ def driver_init(request):
 class BasicTest:
     pass
 class Test_URL(BasicTest):
-
-    # def test_open_urld(self):
-    #     self.driver.get("https://www.lambdatest.com/")
-    #     print(self.driver.title)
-    #     time.sleep(5)
 
 
     def test_open_urls(self):
